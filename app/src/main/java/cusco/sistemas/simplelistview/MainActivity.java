bf7c41072866bc647fc85bde2418d8fecf1c7113
Fernando <cuzcator@gmail.com>
2019-09-02 21:26:34 -0500

    listview
    Agregamos el listview.

--- app/src/main/java/cusco/sistemas/simplelistview/MainActivity.java
+++ app/src/main/java/cusco/sistemas/simplelistview/MainActivity.java
@@ -3,12 +3,34 @@ package cusco.sistemas.simplelistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.idListView);
+
+        //datos que queremos mostrar
+        List<String> names = new ArrayList<String>();
+        names.add("Fernando Cusco");
+        names.add("Maria Zeta");
+        names.add("Santiago Cruz");
+        names.add("Norma Abad");
+
+        //adaptador, forma visual en que se mostraran los datos
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+        //establecemos el adaptador con nuestro ListView
+        list.setAdapter(adapter);
+
+        
     }
 }
